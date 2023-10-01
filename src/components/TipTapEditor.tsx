7bced6665555a4a38b1d1eec525900fbcc40b100
HavsQa <89462067+HavsQa@users.noreply.github.com>
2023-10-01 04:26:40 +0000

    .

--- src/components/TipTapEditor.tsx
+++ src/components/TipTapEditor.tsx
@@ -76,7 +76,7 @@ const TipTapEditor = ({ note }: Props) => {
     <>
       <div className="flex">
         {editor && <TipTapMenuBar editor={editor} />}
-        <Button disabled variant={"outline"}>
+        <Button disabled variant={"destructive"}>
           {saveNote.isLoading ? "Sauvegarde..." : "Sauvegardé"}
         </Button>
       </div>
